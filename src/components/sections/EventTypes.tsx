21bf460d69f6a8b3c16e718fa4d5e966bf0f60e7
sidharthmk24 <sidharthmk24@gmail.com>
2025-12-17 18:03:25 +0530

    img changed

--- src/components/sections/EventTypes.tsx
+++ src/components/sections/EventTypes.tsx
@@ -8,39 +8,39 @@ import ImageShowcaseSwiper from "../ui/newCarousel";
 const EventTypes = () => {
     const eventTypes = [
   {
-    src: "/eventImg/evnt1.png",
+    src: "/evnts/img1.png",
     alt: "Engagement and Pre-Wedding Rituals",
     heading: "Engagement and Pre-Wedding Rituals",
     content:
       "Neat, welcoming spaces suited for meaningful ceremonies held with care and simplicity.",
   },
   {
-    src: "/eventImg/evnt2.png",
+    src: "/evnts/img2.png",
     alt: "Naming Ceremonies",
     heading: "Naming Ceremonies",
     content:
       "A peaceful space for families to celebrate their special milestone in a calm and supportive setting.",
   },
   {
-    src: "/eventImg/evnt3.png",
+    src: "/evnts/img3.png",
     alt: "Small Corporate Meetings",
     heading: "Small Corporate Meetings",
     content:
       "Quiet, well-kept rooms ideal for discussions, training sessions, and professional gatherings.",
   },
   {
-    src: "/eventImg/evnt2.png",
+    src: "/evnts/img1.png",
     alt: "Birthday Celebrations",
     heading: "Birthday Celebrations",
     content:
       "Intimate spaces perfect for celebrating another year of life with close friends and family.",
   },
    {
-    src: "/eventImg/evnt1.png",
+    src: "/evnts/img2.png",
     alt: "Small Corporate Meetings",
     heading: "Small Corporate Meetings",
     content:
-      "Quiet, well-kept rooms ideal for discussions, training sessions, and professional gatherings.",
+      "Quiet, well-kept rooms  ideal for discussions, training sessions, and professional gatherings.",
   },
 ];
 
